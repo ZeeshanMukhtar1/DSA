d532fd12bf548e32892cd3f89072d5ed0296e491
Zeeshan Mukhtar <zeshanmukhtar878@gmail.com>
2024-07-11 20:22:30 +0500

    added initial dsa docs with arrays

---
+++ docs/arrays/introduction.md
@@ -0,0 +1,8 @@
+---
+id: introduction
+title: Introduction to Arrays
+---
+
+## What is an Array in JavaScript?
+
+An array is a data structure that can hold more than one value at a time. It is a collection of elements, each identified by a numeric index.
---
+++ docusaurus.config.js
@@ -0,0 +1,131 @@
+import { themes as prismThemes } from "prism-react-renderer";
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "DSA in JavaScript",
+  tagline: "Data Structures and Algorithms in JavaScript",
+  favicon: "img/js.svg",
+
+  // Set the production url of your site here
+  url: "https://DSA.netlify.app/",
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "zeeshanMukhtar1", // Usually your GitHub org/user name.
+  projectName: "DSA", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: "./sidebars.js",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/zeeshanMukhtar1/DSA",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/zeeshanMukhtar1/DSA",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: "img/docusaurus-social-card.jpg",
+      navbar: {
+        title: "My Site",
+        logo: {
+          alt: "My Site Logo",
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Tutorial",
+          },
+          { to: "/blog", label: "Blog", position: "left" },
+          {
+            href: "https://github.com/facebook/docusaurus",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Tutorial",
+                to: "/docs/intro",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "LinkedIn",
+                href: "https://www.linkedin.com/in/zeeshanmukhtar1/",
+              },
+              {
+                label: "Discord",
+                href: "https://discordapp.com/invite/zeeshanmukhtar1",
+              },
+              {
+                label: "Instagram",
+                href: "https://www.instagram.com/zeshanmukhtar01/",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "Blog",
+                to: "/blog",
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/zeeshanMukhtar1/",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} DSA in Javascript, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
